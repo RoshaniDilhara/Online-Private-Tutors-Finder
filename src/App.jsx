92c5d3c6b09a7ce777c2a3499a9d9beca8107d99
Dulanjali <dulapreethi@gmail.com>
2020-04-05 16:42:58 +0530

    Tutor login design

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Navbar from "./Components/CustomNavBar";
 import AdminLogin from "./Components/AdminLogin";
 import StudentLogin from "./Components/StudentLogin";
 import TutorLogin from "./Components/TutorLogin";
+import TutorSignUp from "./Components/TutorSignUp";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path="/adminlogin" component={AdminLogin} />
         <Route path="/studentlogin" component={StudentLogin} />
         <Route path="/tutorlogin" component={TutorLogin} />
+        <Route path="/signup" component={TutorSignUp} />
       </div>
     </Router>
   );
